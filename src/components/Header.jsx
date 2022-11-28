f79d5a380f7ca0e4d7c80a8a2ba07451b0b12f12
autumnly1007 <autumnly1007@gmail.com>
2022-11-28 17:23:20 +0900

    Add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './header.scss';
+
+export default function Header() {
+  return (
+    <div className='header-wrap'>
+      <div className='header-container'>
+        <div className='services'>서비스</div>
+        <div className='search'>검색</div>
+        <div className='nav'>내브바</div>
+      </div>
+    </div>
+  );
+}
