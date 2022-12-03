6c42cfd0c1faef51ceadc1df8644428b3f91934b
autumnly1007 <autumnly1007@gmail.com>
2022-12-03 19:38:50 +0900

    Add admin requests

--- src/api/requests.js
+++ src/api/requests.js
@@ -40,7 +40,6 @@ const requestGet = async (path, instance) => {
   } catch (e) {
     // TODO: 에러 처리 추가
     console.log(e);
-    // TODO: 에러 처리 추가
     throw new Error('에러가 발생하였습니다.');
   } finally {
   }
@@ -54,7 +53,6 @@ const requestPut = async (path, instance, data) => {
   } catch (e) {
     // TODO: 에러 처리 추가
     console.log(e);
-    // TODO: 에러 처리 추가
     throw new Error('에러가 발생하였습니다.');
   } finally {
   }
@@ -68,11 +66,15 @@ const requestDelete = async (path, instance) => {
   } catch (e) {
     // TODO: 에러 처리 추가
     console.log(e);
-    // TODO: 에러 처리 추가
     throw new Error('에러가 발생하였습니다.');
   } finally {
   }
 };
+
+// 로그인 /auth/login, POST
+// 인증확인 /auth/me, POST
+// 로그아웃 /auth/logout, POST
+// 사용자 정보 수정 /auth/user, PUT
 
 // 회원가입
 export const signup = (data) => {
@@ -95,6 +97,11 @@ export const updateUserInfo = (data) => {
   return requestGet(AUTH_USER, authInstance, data);
 };
 
+// 선택 가능한 은행 목록 조회 /account/banks, GET
+// 계좌 목록 및 잔액 조회 /account, GET
+// 계좌 연결 /account, POST
+// 계좌 해지 /account, DELETE
+
 // 선택 가능한 은행 목록 조회
 export const selectBanks = () => {
   return requestGet(BANKS, authInstance);
@@ -109,29 +116,16 @@ export const insertAccount = () => {};
 // 계좌 해지
 export const deleteAccount = () => {};
 
-// 모든 제품 조회 (관리자)
-export const selectListProductAdmin = () => {
-  return requestGet(PRODUCT, adminInstance);
-};
-
-// 전체 거래 내역 조회 (관리자)
-export const selectListOrderAdmin = () => {};
-
-// 거래 내역 완료/완료 해제, 취소/취소 해제
-export const updateOrderAdmin = () => {};
-
-// 제품 추가
-export const insertProduct = () => {};
-
-// 제품 수정
-export const updateProduct = () => {};
-
-// 제품 삭제
-export const deleteProduct = () => {};
+// 단일 제품 상세 조회 /products/:productId, GET
+// 제품 검색 /products/search, POST
+// 제품 거래 신청 /products/buy, POST
+// 제품 거래 확정 /products/ok, POST
+// 제품 전체 거래 내역 /products/transactions/details, GET
+// 단일 제품 상세 거래 내역 /products/transactions/detail, POST
 
 // 단일 제품 상세 조회
 export const selectProductDetail = (id) => {
-  return requestGet(PRODUCT + `/${id}`, authInstance);
+  return requestGet(`${PRODUCT}/${id}`, authInstance);
 };
 
 // 제품 검색
@@ -151,16 +145,6 @@ export const selectListOrder = () => {};
 // 단일 제품 상세 거래 내역
 export const selectOrder = () => {};
 
-// 로그인 /auth/login, POST
-// 인증확인 /auth/me, POST
-// 로그아웃 /auth/logout, POST
-// 사용자 정보 수정 /auth/user, PUT
-
-// 선택 가능한 은행 목록 조회 /account/banks, GET
-// 계좌 목록 및 잔액 조회 /account, GET
-// 계좌 연결 /account, POST
-// 계좌 해지 /account, DELETE
-
 // 모든 제품 조회 /products, GET
 // 전체 거래 내역 /products/transactions/all, GET
 // 거래 내역 완료/취소 및 해제 /products/transactions/:detailId, PUT
@@ -168,10 +152,32 @@ export const selectOrder = () => {};
 // 제품 수정 /products/:productId, PUT
 // 제품 삭제 /products/:productId, DELETE
 
-// 단일 제품 상세 조회 /products/:productId, GET
-// 제품 검색 /products/search, POST
+// 모든 제품 조회 (관리자)
+export const selectListProductAdmin = () => {
+  return requestGet(PRODUCT, adminInstance);
+};
 
-// 제품 거래 신청 /products/buy, POST
-// 제품 거래 확정 /products/ok, POST
-// 제품 전체 거래 내역 /products/transactions/details, GET
-// 단일 제품 상세 거래 내역 /products/transactions/detail, POST
+// 전체 거래 내역 조회 (관리자)
+export const selectListOrderAdmin = () => {
+  return requestGet(TRANSACTION_ALL, adminInstance);
+};
+
+// 거래 내역 완료/완료 해제, 취소/취소 해제 (관리자)
+export const updateOrderAdmin = (id, data) => {
+  return requestPut(`${TRANSACTION}/${id}`, adminInstance, data);
+};
+
+// 제품 추가 (관리자)
+export const insertProduct = (data) => {
+  return requestPost(PRODUCT, adminInstance, data);
+};
+
+// 제품 수정 (관리자)
+export const updateProduct = (id, data) => {
+  return requestPut(`${PRODUCT}/${id}`, adminInstance, data);
+};
+
+// 제품 삭제 (관리자)
+export const deleteProduct = (id) => {
+  return requestDelete(`${PRODUCT}/${id}`, adminInstance);
+};
