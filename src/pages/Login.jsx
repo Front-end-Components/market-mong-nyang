9e4888f0a693b5f262c9d9deefd85fe49e91ab2e
autumnly1007 <autumnly1007@gmail.com>
2022-12-03 19:27:37 +0900

    Add test code in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { login } from '../api/requests';
 import Button from '../components/Button';
 
 export default function Login() {
-  const [text, setText] = useState('');
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {};
-
+  // TEST
   const handleSubmit = (e) => {
     e.preventDefault();
-    setText('');
-    navigate(`/`);
+    const test = {
+      email: 'test@test.com',
+      password: '1q2w3e4r',
+    };
+    const res = login(test);
+    console.log(res);
   };
 
   return (
     <div>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <input type='text' onChange={handleChange} />
+        <input type='text' />
       </form>
-      <Button name='로그인'></Button>
+      <Button name='로그인' onClick={handleSubmit}></Button>
     </div>
   );
 }
