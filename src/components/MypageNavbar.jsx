dc9386e1e6be3874666b14bb819e59542bd6816c
autumnly1007 <autumnly1007@gmail.com>
2022-12-03 20:26:46 +0900

    Update mypage navbar

--- src/components/MypageNavbar.jsx
+++ src/components/MypageNavbar.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import style from './MypageNavbar.module.scss';
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
 export default function MypageNavbar() {
+  const location = useLocation();
   return (
     <div className={style.mypage}>
       <h1>마이페이지</h1>
       <nav className={style.mypageNav}>
-        <Link to='/mypage/order'>주문 내역</Link>
-        <Link to='/mypage/account'>계좌 관리</Link>
-        <Link to='/mypage/like'>찜한 상품</Link>
-        <Link to='/mypage/info'>개인 정보 수정</Link>
+        <ul>
+          <li className={location.pathname === '/mypage/order' ? style.active : ''}>
+            <Link to='/mypage/order'>
+              <span>주문 내역</span>
+              <MdOutlineArrowForwardIos fontSize='12' color='rgb(95, 0, 128)' />
+            </Link>
+          </li>
+          <li className={location.pathname === '/mypage/account' ? style.active : ''}>
+            <Link to='/mypage/account'>
+              <span>계좌 관리</span>
+              <MdOutlineArrowForwardIos fontSize='12' color='rgb(95, 0, 128)' />
+            </Link>
+          </li>
+          <li className={location.pathname === '/mypage/like' ? style.active : ''}>
+            <Link to='/mypage/like'>
+              <span>찜한 상품</span>
+              <MdOutlineArrowForwardIos fontSize='12' color='rgb(95, 0, 128)' />
+            </Link>
+          </li>
+          <li className={location.pathname === '/mypage/info' ? style.active : ''}>
+            <Link to='/mypage/info'>
+              <span>개인 정보 수정</span>
+              <MdOutlineArrowForwardIos fontSize='12' color='rgb(95, 0, 128)' />
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   );
