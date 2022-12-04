044c3cb549eb164251aab46885da3ed78b6b8718
autumnly1007 <autumnly1007@gmail.com>
2022-12-04 15:30:31 +0900

    Update global variables scss

--- src/pages/MyLike.jsx
+++ src/pages/MyLike.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import MypageHeader from '../components/MypageHeader';
 import style from './MyLike.module.scss';
+import { VscHeart } from 'react-icons/vsc';
 
 export default function MyLike() {
   return (
     <div className={style.myLike}>
       <MypageHeader name={'찜한 상품'} />
+      <div className={style.content}>
+        <VscHeart size='60' title='찜' color='lightgray' />
+        <p>찜한 상품이 없습니다.</p>
+      </div>
     </div>
   );
 }
