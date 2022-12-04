7196cb00ae3324c4c779c7917e3f0c60c455cb6c
autumnly1007 <autumnly1007@gmail.com>
2022-12-04 19:07:16 +0900

    Update constants path

---
+++ src/constants/path.js
@@ -0,0 +1,17 @@
+const PATH = {
+  SIGNUP: '/auth/signup',
+  LOGIN: '/auth/login',
+  AUTH_ME: '/auth/me',
+  AUTH_USER: '/auth/user',
+  BANKS: '/account/banks',
+  ACCOUNT: '/account',
+  PRODUCT: '/products',
+  SEARCH: '/products/search',
+  BUY: '/products/buy',
+  BUY_OK: '/products/ok',
+  TRANSACTION: '/products/transactions',
+  TRANSACTION_ALL: '/products/transactions/all',
+  TRANSACTION_DETAIL: '/products/transactions/details',
+};
+
+export default PATH;
