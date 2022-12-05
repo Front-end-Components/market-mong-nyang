0fbb81908ed0ccc49506b561387c8d6d444518ad
plou102 <plou102@naver.com>
2022-12-05 17:30:49 +0900

    update MyOrder.jsx & scss

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import style from './MyOrder.module.scss';
+import MypageHeader from '../components/MypageHeader';
 
 export default function MyOrder() {
-  return <div>주문 내역</div>;
+  return (
+    <div className={style.myLike}>
+      <MypageHeader name={'주문 내역'} />
+      <div className={style.content}>
+      </div>
+    </div>
+  );
 }
