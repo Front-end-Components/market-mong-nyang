fe2b41b0e9ac3ddf2a799f7b1b7a6a5b28245303
jisooround <9utesleben@gmail.com>
2022-12-07 17:15:13 +0900

    Update import Item component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import style from './Products.module.scss';
+import { orders, products } from '../data/data.js';
+import Item from '../components/Item.jsx'
 
 export default function AllProducts() {
-  return <div>AllProducts</div>;
+
+  let [item] = useState(products);
+  console.log(item[0].title);
+
+  return (
+    <div className={style.products}>
+      AllProducts
+      <div className={style.container}>
+        <div className={style.row}>
+          {
+            item.map((a, i) => {
+              return (
+                  <Item item={item[i]} />
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
