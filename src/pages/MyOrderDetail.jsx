962841c284dc1ff81fce8c9e500aca9f55be6aee
plou102 <plou102@naver.com>
2022-12-08 21:31:41 +0900

    edit detail page

--- src/pages/MyOrderDetail.jsx
+++ src/pages/MyOrderDetail.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import MypageHeader from '../components/MypageHeader';
+import style from './MyOrderDtail.module.scss';
 
 export default function MyOrderDetail() {
   const { orderId } = useParams();
-  return <div>MyOrderDetail {orderId}</div>;
+  return (
+    <div className={style.MyOrderDetail}>
+      <MypageHeader name={'주문 상세정보'} />
+      <div className={style.content}>
+      </div>
+    </div>
+  );
 }
