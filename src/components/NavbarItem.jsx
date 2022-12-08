f728987b114b2d8259b6efec6cac3450e49e1c8e
autumnly1007 <autumnly1007@gmail.com>
2022-12-08 15:53:59 +0900

    Update navbar item active

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -6,10 +6,10 @@ import style from './NavbarItem.module.scss';
 export default function NavbarItem({ page, title }) {
   const location = useLocation();
   return (
-    <li className={location.pathname === page ? style.active : ''}>
+    <li className={location.pathname.split('/')[2] === page.split('/')[2] ? style.active : ''}>
       <Link to={page}>
         <span>{title}</span>
-        <MdOutlineArrowForwardIos fontSize='12' color='rgb(95, 0, 128)' />
+        <MdOutlineArrowForwardIos size='12' color='rgb(95, 0, 128)' />
       </Link>
     </li>
   );
