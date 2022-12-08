8e7deca2b950f909dc44263b928618feaf98ec98
autumnly1007 <autumnly1007@gmail.com>
2022-12-08 15:53:30 +0900

    Update admin products

--- src/pages/admin/Products.jsx
+++ src/pages/admin/Products.jsx
@@ -1,5 +1,22 @@
+import { selectListProductAdmin } from '@/api/requests';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './Products.module.scss';
 
 export default function Products() {
-  return <div>Products</div>;
+  const res = selectListProductAdmin();
+  return (
+    <div>
+      <div className={style.buttons}>
+        <button>삭제</button>
+        <Link to='/admin/products/add'>
+          <button>등록</button>
+        </Link>
+      </div>
+      {res.data || <div>등록된 상품이 없습니다.</div>}
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
