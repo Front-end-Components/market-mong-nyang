09bfa11f5b8ee55bab1d3a53e5b36d6c4b924794
plou102 <plou102@naver.com>
2022-12-09 18:31:14 +0900

    edit detail page - api

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,41 +1,32 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import style from './MyOrder.module.scss';
 import MypageHeader from '../components/MypageHeader';
-import {selectListOrder} from '../api/requests.js';
-import Button from '@/components/Button';
+import {selectListOrder} from '@/api/requests';
+import Order from '@/components/Order'
+
 
 export default function MyOrder() {
-  let detailsRes = selectListOrder();
-  console.log(detailsRes);
+  const [details, setDetails] = useState([]);
 
-  const location = useLocation();
+  useEffect(() => {
+    async function getData() {
+      const data = await selectListOrder();
+      setDetails(data);
+    }
+    getData();
+  }, []);
+
   return (
     <div className={style.myOrder}>
-    <MypageHeader name={'주문 내역 (예시 임돵)'} />
-    <div className={style.content}>
-      <div className={style.orderContent}>
-        <div className={style.imgContent}>
-          <img src='https://cdn-pro-web-241-106-godomall.spdycdn.net/bienbien3_godomall_com/data/goods/22/11/48/1000000897/1000000897_main_086.jpg' className={style.orderImg}></img>
-        </div>
-        <div className={style.textContent}>
-          <p className={location.pathname === '/mypage/order/id'}>
-            <Link to='/mypage/order/id'>
-            <span className={style.productName}>상품이름 (클릭하면 상세 페이지로 긔긔)</span>
-            </Link>
-            </p>
-          <p className={style.orderPrice}>상품 가격</p>
-          <p className={style.orderDate}>구매 날짜</p>
-          <p className={style.orderState}>구매 완료 (임시)</p>
-          <p className={style.orderGuide}>구매가 완료 되었습니다.</p>
-          <p className={style.orderGuide}>구매 확정 이후에는 주문 취소가 불가능합니다.</p>
-        </div>
-        <div className={style.btnContent}>
-          <Button className={style.orderCancel} name={'주문 취소'} />
-          <Button className={style.orderOk} name={'구매 확정'} isPurple={true} />
-        </div>
-      </div>
-    </div>
+    <MypageHeader name={'주문 내역 (썸네일 교체해야 함 + 주문 상태)'} />
+
+    {Array.isArray(details) ? (
+    details.map((item) => {
+      return <Order key={item.id} item={item} />;
+    })
+  ) : (
+    <p>구매하시 상품이 없습니다.</p>
+  )}
     </div>
   );
 }
