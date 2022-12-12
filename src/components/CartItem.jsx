3a5e2b95856c436181d6c0fff4d9cfdcad3ddf12
jisooround <9utesleben@gmail.com>
2022-12-12 17:09:38 +0900

    Fix cartItem component 임시로 합침

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import { useState } from 'react';
 import style from './CartItem.module.scss';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { formatPrice } from '@/utils/formats';
 
-export default function CartItem() {
-
-  let list = useSelector((state) => state.cart );
-  const price = list[0].price;
+export default function CartItem(props) {
+  console.log('hello');
+  // console.log(props.list);
+  // let list = useSelector((state) => state.cart);
+  const price = props.list.price;
   const [count, setCount] = useState(1);
+
 
   return (
     <div className={style.cartitem}>
           <div className={style.infoarea}> 
             <div className={style.checkbox}><input type="checkbox" /></div>
-            <div className={style.thumbnail}><img src={list[0].thumbnail} alt={list[0].title} /></div>
-            <div className={style.name}>{list[0].title}</div>
+            <div className={style.thumbnail}><img src={props.list.thumbnail} alt={props.list.title} /></div>
+            <div className={style.name}>{props.list.title}</div>
           </div>
           <div className={style.calcarea}>
             <div className={style.countwrap}>
-            <div className={style.count}>
-              <button onClick={() => {
-                if(count === 1){
-                  return 1;
-                }
-                setCount(count - 1);
-              }}>-</button>
-              <p>{count}</p>
-              <button onClick={() => {
-                setCount(count + 1);
-              }}>+</button>
-            </div>
+              <div className={style.count}>
+                <button onClick={() => {
+                  if(count === 1){
+                    return 1;
+                  }
+                  setCount(count - 1);
+                }}>-</button>
+                <p>{count}</p>
+                <button onClick={() => {
+                  setCount(count + 1);
+                }}>+</button>
+              </div>
             </div>
             <div className={style.price}>{formatPrice(price)}원</div>
           </div>
