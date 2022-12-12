eb0362904c236f087cea7c5e808dacfd17870547
autumnly1007 <autumnly1007@gmail.com>
2022-12-12 17:38:13 +0900

    Fix Cart, CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import { useState } from 'react';
 import style from './CartItem.module.scss';
-// import { useSelector } from 'react-redux';
 import { formatPrice } from '@/utils/formats';
 
-export default function CartItem(props) {
-  console.log('hello');
-  // console.log(props.list);
-  // let list = useSelector((state) => state.cart);
-  const price = props.list.price;
+export default function CartItem({ item }) {
   const [count, setCount] = useState(1);
-
 
   return (
     <div className={style.cartitem}>
-          <div className={style.infoarea}> 
-            <div className={style.checkbox}><input type="checkbox" /></div>
-            <div className={style.thumbnail}><img src={props.list.thumbnail} alt={props.list.title} /></div>
-            <div className={style.name}>{props.list.title}</div>
+      <div className={style.infoarea}>
+        <div className={style.checkbox}>
+          <input type='checkbox' />
+        </div>
+        <div className={style.thumbnail}>
+          <img src={item.thumbnail} alt={item.title} />
+        </div>
+        <div className={style.name}>{item.title}</div>
+      </div>
+      <div className={style.calcarea}>
+        <div className={style.countwrap}>
+          <div className={style.count}>
+            <button
+              onClick={() => {
+                if (count === 1) {
+                  return 1;
+                }
+                setCount(count - 1);
+              }}
+            >
+              -
+            </button>
+            <p>{count}</p>
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </button>
           </div>
-          <div className={style.calcarea}>
-            <div className={style.countwrap}>
-              <div className={style.count}>
-                <button onClick={() => {
-                  if(count === 1){
-                    return 1;
-                  }
-                  setCount(count - 1);
-                }}>-</button>
-                <p>{count}</p>
-                <button onClick={() => {
-                  setCount(count + 1);
-                }}>+</button>
-              </div>
-            </div>
-            <div className={style.price}>{formatPrice(price)}원</div>
-          </div>
+        </div>
+        <div className={style.price}>{formatPrice(item.price)} 원</div>
+      </div>
     </div>
   );
-};
+}
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,54 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
 import MypageHeader from '../components/MypageHeader';
-// import CartItem from '../components/CartItem';
+import CartItem from '../components/CartItem';
 import style from './Cart.module.scss';
 import { useSelector } from 'react-redux';
-import { formatPrice } from '@/utils/formats';
-
 
 export default function Cart() {
-  let list = useSelector((state) => state.cart );
-  console.log(list);
-
-  const price = list.price;
-  const [count, setCount] = useState(1);
+  const list = useSelector((state) => state.cart);
 
   return (
     <div className={style.cart}>
-     <MypageHeader name={'장바구니'} />
-     <div className={style.container}>
-      {
-        list.map((a, i) => {
-            // <CartItem i={i} />
-            // console.log(i)
-            <div className={style.cartitem}>
-              <div className={style.infoarea}> 
-                <div className={style.checkbox}><input type="checkbox" /></div>
-                <div className={style.thumbnail}><img src={list[i].thumbnail} alt={list[i].title} /></div>
-                <div className={style.name}>{list[i].title}</div>
-              </div>
-              <div className={style.calcarea}>
-                <div className={style.countwrap}>
-                  <div className={style.count}>
-                    <button onClick={() => {
-                      if(count === 1){
-                        return 1;
-                      }
-                      setCount(count - 1);
-                    }}>-</button>
-                    <p>{count}</p>
-                    <button onClick={() => {
-                      setCount(count + 1);
-                    }}>+</button>
-                  </div>
-                </div>
-                <div className={style.price}>{formatPrice(price)}원</div>
-              </div>
-            </div>
-        })
-      }
-     </div>
+      <MypageHeader name={'장바구니'} />
+      <div className={style.container}>
+        {list.map((item) => {
+          return <CartItem key={item.id} item={item} />;
+        })}
+      </div>
     </div>
   );
 }
