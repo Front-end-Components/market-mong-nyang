a811af92c719ceace40d2c46efd1ef9e1064c8a1
soyun <soyoon4122@naver.com>
2022-12-12 17:25:56 +0900

    Update Account, Payment

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './Account.module.scss';
-// import { useEffect, useState } from "react";
-import { account } from '../data/data';
+import { selectListAccount } from '@/api/requests';
+import { account } from '@/data/data';
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import { Pagination } from "swiper";
+import "swiper/scss"; //basic
+import "swiper/scss/pagination";
 
 export default function Account() {
   //let [계좌, 계좌변경] = useState(selectBanks);
-  console.log(account.accounts);
+  console.log(selectListAccount());
+
+  const [accountData, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const data = await selectListAccount();
+      setProducts(data);
+    }
+    getData();
+  }, []);
+  
   return (
-  <div>
-    
-    <div className={style.accountContent}>
-      <span className={style.title}>계좌 간편결제</span>
-      {/* 스위퍼 */}
-      <div className={style.slide}>
-        <ul>
-          {
-            account.accounts.map((a, i) => {
-              return (
-                <li className={style.list} i={i} key={i}>
-                  <h4>{account.accounts[i].bankName}</h4>
-                  <p>{account.accounts[i].accountNumber}</p>
-                  <span>{account.accounts[i].balance}</span>
-                </li>
-              )
-            })
-          }
-          {/* 계좌목록 */}
-          {/* 계좌등록 */}
-        </ul>
-      </div>
+  <div className={style.accountContent}>
+    <span className={style.title}>계좌 간편결제</span>
+    {/* 스위퍼 */}
+    <div className={style.slide}>
+      <Swiper
+        modules = {[Pagination]}
+        pagination={{ clickable : true }}
+        loop={true} // 루프 슬라이드
+        spaceBetween={10} // 슬라이드간의 간격
+        slidesPerView={2} // 한 번에 보여지는 슬라이드 개수
+        style={{
+          "--swiper-pagination-color": "#43007c"
+        }}
+      >
+        {
+          account.accounts.map((a, i) => {
+            return (
+              <SwiperSlide onClick i={i} key={i} className={style.list}>
+                <h4>{account.accounts[i].bankName}</h4>
+                <p>{account.accounts[i].accountNumber}</p>
+                <span>{account.accounts[i].balance}</span>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </div>
   </div>
   )
