8fc73133b25f19dda4ad935fac192fb6fba8a70f
autumnly1007 <autumnly1007@gmail.com>
2022-12-12 20:57:52 +0900

    Update admin product detail useState

--- src/pages/admin/ProductDetail.jsx
+++ src/pages/admin/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { deleteProduct, selectProductDetail } from '@/api/requests';
+import { deleteProduct, getProductDetail } from '@/api/requests';
 import Button from '@/components/Button';
 import { formatPrice } from '@/utils/formats';
 import React, { useEffect, useState } from 'react';
@@ -7,24 +7,20 @@ import style from './ProductDetail.module.scss';
 
 export default function ProductDetail() {
   const navigate = useNavigate();
-  // const [product, setProduct] = useState({});
-  // const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
 
-  // useEffect(() => {
-  //   async function getData() {
-  //     const data = await selectProductDetail(id);
-  //     setProduct(data);
-  //   }
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    async function getData() {
+      const data = await getProductDetail(id);
+      setProduct(data);
+    }
+    getData();
+  }, []);
 
-  const {
-    state: {
-      item: { id, title, price, description, tags, thumbnail, photo, isSoldOut },
-    },
-  } = useLocation();
-
-  const onClickUpdateBtn = () => {};
+  const onClickUpdateBtn = () => {
+    navigate(`/admin/products/modify/${product.id}`, { state: { product } });
+  };
 
   const onClickdDeleteBtn = () => {
     if (window.confirm('해당 상품을 삭제하시겠습니까?')) {
@@ -41,13 +37,13 @@ export default function ProductDetail() {
 
   return (
     <div>
-      <img src='' alt='thumbnail' />
-      <p>상품명 : {title}</p>
-      <p>가격 : {formatPrice(price)}</p>
-      <p>상품 설명 : {description}</p>
-      <p>태그 : {tags}</p>
-      <p>품절 여부 : {isSoldOut ? 'Y' : 'N'}</p>
-      <img src='' alt='photo' />
+      <img src={product.thumbnail} alt='thumbnail' />
+      <p>상품명 : {product.title}</p>
+      <p>가격 : {formatPrice(product.price)}</p>
+      <p>상품 설명 : {product.description}</p>
+      <p>태그 : {product.tags}</p>
+      <p>품절 여부 : {product.isSoldOut ? 'Y' : 'N'}</p>
+      <img src={product.photo} alt='photo' />
       <div className={style.buttons}>
         <Button name={'수정'} isPurple={true} onClick={onClickUpdateBtn} />
         <Button name={'삭제'} isPurple={true} onClick={onClickdDeleteBtn} />
