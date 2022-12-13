3d25bf0bed61d1f6f85f4e3ae4b3aee69b5bb518
soyun <soyoon4122@naver.com>
2022-12-13 15:48:52 +0900

    Update Account, Payment page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import style from './Account.module.scss';
 import { selectListAccount } from '@/api/requests';
-import { account } from '@/data/data';
 import { Swiper, SwiperSlide } from "swiper/react"; // basic
 import { Pagination } from "swiper";
 import "swiper/scss"; //basic
 import "swiper/scss/pagination";
 
 export default function Account() {
-  //let [계좌, 계좌변경] = useState(selectBanks);
-  console.log(selectListAccount());
-
-  const [accountData, setProducts] = useState([]);
+  const [account, setAccounts] = useState([]);
 
   useEffect(() => {
     async function getData() {
       const data = await selectListAccount();
-      setProducts(data);
+      setAccounts(data);
     }
     getData();
   }, []);
@@ -24,29 +20,33 @@ export default function Account() {
   return (
   <div className={style.accountContent}>
     <span className={style.title}>계좌 간편결제</span>
-    {/* 스위퍼 */}
+    <p className={style.totalBalance}>총 계좌 잔액: <span>{account.totalBalance}</span></p>
     <div className={style.slide}>
       <Swiper
         modules = {[Pagination]}
         pagination={{ clickable : true }}
-        loop={true} // 루프 슬라이드
+        loop={false} // 루프 슬라이드
         spaceBetween={10} // 슬라이드간의 간격
-        slidesPerView={2} // 한 번에 보여지는 슬라이드 개수
+        slidesPerView={1} // 한 번에 보여지는 슬라이드 개수
         style={{
           "--swiper-pagination-color": "#43007c"
         }}
       >
-        {
-          account.accounts.map((a, i) => {
+        {Array.isArray(account.accounts) ? (
+          account.accounts.map((item, idx) => {
             return (
-              <SwiperSlide onClick i={i} key={i} className={style.list}>
-                <h4>{account.accounts[i].bankName}</h4>
-                <p>{account.accounts[i].accountNumber}</p>
-                <span>{account.accounts[i].balance}</span>
+              <SwiperSlide onClick item={item.idx} key={idx} className={style.list}>
+                <h4>{item.bankName}</h4>
+                <p>{item.accountNumber}</p>
+                <span>{item.balance}</span>
               </SwiperSlide>
             )
           })
-        }
+        ) : (
+          <SwiperSlide className={style.list}>
+            <h4>등록된 계좌가 없습니다.</h4>
+          </SwiperSlide>
+        )}
       </Swiper>
     </div>
   </div>
