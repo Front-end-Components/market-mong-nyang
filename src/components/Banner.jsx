55ae4ad260cca198f712c7524c76731a9a38e729
jisooround <9utesleben@gmail.com>
2022-12-14 00:09:09 +0900

    Update main banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,50 +4,49 @@ import { Swiper, SwiperSlide } from "swiper/react"; // basic
 import { Pagination, Navigation, Autoplay } from "swiper";
 import "swiper/scss"; //basic
 import "swiper/scss/pagination";
-import "swiper/scss/navigation";
+// import "swiper/scss/navigation";
 
 
 export default function Banner() {
   return (
     <div className={style.banner}>
       <Swiper
-      modules = {[Pagination, Navigation, Autoplay]}
+      modules = {[Pagination, Autoplay]}
       pagination={{ clickable : true }}
       loop={true} // 루프 슬라이드
       autoplay={{ delay: 4000 }} // 자동 슬라이드
       spaceBetween={0} // 슬라이드간의 간격
       slidesPerView={1} // 한 번에 보여지는 슬라이드 개수
-      navigation
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
       style={{
         "--swiper-navigation-color": "#43007c",
-        "--swiper-pagination-color": "#43007c"
+        "--swiper-pagination-color": "#43007c",
       }}
     >
     <SwiperSlide className={style.slide}>
       <a href="#">
-      <img src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/b474b6a1-5077-4800-947d-6e7b1929a37a.jpg" alt="" className={style.image}/>
+      <img src="/images/main-banner-01.jpg" alt="" className={style.image}/>
       </a>
       </SwiperSlide>
     <SwiperSlide className={style.slide}>
       <a href="">
-      <img src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/b474b6a1-5077-4800-947d-6e7b1929a37a.jpg" alt="" className={style.image}/>
+      <img src="/images/main-banner-02.jpg" alt="" className={style.image}/>
       </a>
     </SwiperSlide>
     <SwiperSlide className={style.slide}>
-      <a href="">
-      <img src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/b474b6a1-5077-4800-947d-6e7b1929a37a.jpg" alt="" className={style.image} />
+      <a href="products/DNrJxxfgQWCcnRE7BKBJ">
+      <img src="/images/main-banner-03.jpg" alt="" className={style.image} />
       </a>
     </SwiperSlide>
     <SwiperSlide className={style.slide}>
-      <a href="">
-      <img src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/a916f9a1-e2e2-4600-b856-206490758140.jpg" alt="" className={style.image} />
+      <a href="/products/9vTncKT8yKKsTlc9kkNN">
+      <img src="/images/main-banner-04.jpg" alt="" className={style.image} />
       </a>
     </SwiperSlide>
     <SwiperSlide className={style.slide}>
-      <a href="">
-      <img src="https://product-image.kurly.com/cdn-cgi/image/format=auto/banner/main/pc/img/20139e33-d871-4de9-a2e8-18a3024af36d.jpg" alt="" className={style.image} />
+      <a href="/products/3cO9tjuAdcjWA1lKfb94">
+      <img src="/images/main-banner-05.jpg" alt="" className={style.image} />
       </a>
     </SwiperSlide>
     </Swiper>
