47f06466ab591ab2a04a4f22d2d0106bb59680ad
autumnly1007 <autumnly1007@gmail.com>
2022-12-13 14:17:39 +0900

    Add admin order cancel, done function

--- src/pages/admin/OrderDetail.jsx
+++ src/pages/admin/OrderDetail.jsx
@@ -1,17 +1,53 @@
+import { updateOrderAdmin } from '@/api/requests';
 import Button from '@/components/Button';
 import { formatDate, formatPrice } from '@/utils/formats';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import style from './OrderDetail.module.scss';
 
 export default function OrderDetail() {
   const navigate = useNavigate();
+  const [cancled, setCancled] = useState(false);
+  const [finished, setFinished] = useState(false);
 
-  const {
+  let {
     state: {
       item: { detailId, user, account, product, timePaid, isCanceled, done },
     },
   } = useLocation();
+
+  useEffect(() => {
+    setCancled(isCanceled);
+    setFinished(done);
+  }, []);
+
+  const handleChangeCancle = () => {
+    const msg = cancled ? '거래 취소를 해제 처리하시겠습니까?' : '거래를 취소 처리하시겠습니까?';
+    if (window.confirm(msg)) {
+      updateOrderAdmin(detailId, { isCanceled: !cancled }).then((res) => {
+        if (res) {
+          setCancled(!cancled);
+          alert('처리가 완료되었습니다.');
+        } else {
+          alert('처리가 완료되지 못했습니다.');
+        }
+      });
+    }
+  };
+
+  const handelChangeDone = () => {
+    const msg = finished ? '거래 완료를 해제 처리하시겠습니까?' : '거래를 완료 처리하시겠습니까?';
+    if (window.confirm(msg)) {
+      updateOrderAdmin(detailId, { done: !finished }).then((res) => {
+        if (res) {
+          setFinished(!finished);
+          alert('처리가 완료되었습니다.');
+        } else {
+          alert('처리가 완료되지 못했습니다.');
+        }
+      });
+    }
+  };
 
   return (
     <div>
@@ -29,12 +65,12 @@ export default function OrderDetail() {
       <p>상품태그 : {product.tags}</p>
       <p>상품 썸네일 : {product.thumbnail}</p>
       <p>거래 일시 : {formatDate(timePaid)}</p>
-      <p>취소여부 : {isCanceled ? 'Y' : 'N'}</p>
-      <p>완료 여부 : {done ? 'Y' : 'N'}</p>
+      <p>취소여부 : {cancled ? 'Y' : 'N'}</p>
+      <p>완료 여부 : {finished ? 'Y' : 'N'}</p>
       <img src='' alt='photo' />
       <div className={style.buttons}>
-        {isCanceled ? <Button name={'거래 취소 해제'} /> : <Button name={'거래 취소'} isPurple={true} />}
-        {done ? <Button name={'거래 완료 해제'} /> : <Button name={'거래 완료'} isPurple={true} />}
+        {cancled ? <Button name={'거래 취소 해제'} onClick={handleChangeCancle} /> : <Button name={'거래 취소'} isPurple={true} onClick={handleChangeCancle} />}
+        {finished ? <Button name={'거래 완료 해제'} onClick={handelChangeDone} /> : <Button name={'거래 완료'} isPurple={true} onClick={handelChangeDone} />}
       </div>
     </div>
   );
