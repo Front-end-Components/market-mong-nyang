48bf5bcd52f04f01ea66f9280f1a6c9dbee40b0d
plou102 <plou102@naver.com>
2022-12-14 12:53:40 +0900

    edit order detail

--- src/pages/MyOrderDetail.jsx
+++ src/pages/MyOrderDetail.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { useLocation} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import MypageHeader from '@/components/MypageHeader';
 import style from './MyOrderDetail.module.scss';
 import { selectOrder } from '@/api/requests';
-import { useEffect } from 'react';
-import { formatPrice } from '@/utils/formats';
 
 export default function MyOrderDetail() {
   const [orderDetail, setOrderDetail] = useState([]);
-  let location = useLocation();
+  
+  const location = useLocation();
   const detailID = {
     'detailId': location.state
   };
@@ -21,15 +20,15 @@ export default function MyOrderDetail() {
     postData();
   }, []);
 
-  let date = orderDetail.timePaid.substr(0, 10);
-  let price = formatPrice(orderDetail.product.price);
+  let date = String(orderDetail.timePaid).substring(0, 10);
+
 
   return (
     <div className={style.MyOrderDetail}>
       <MypageHeader name={'주문 상세정보 test'} />
       <div className={style.content}>
-        <p className={style.orderDate}>주문 날짜 : {date}</p>
-        <p>주문 번호 : {orderDetail.detailId}</p>
+        <span className={style.orderDate}>주문 날짜 : <b>{date}</b></span>
+        <span className={style.orderNum}>주문 번호 : {orderDetail.detailId}</span>
       </div>
     </div>
   );
