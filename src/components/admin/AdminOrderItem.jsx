709e1204ed8b3b575d9b7533e908c239c5a589e2
autumnly1007 <autumnly1007@gmail.com>
2022-12-16 15:18:15 +0900

    Test Commit

--- src/components/admin/AdminOrderItem.jsx
+++ src/components/admin/AdminOrderItem.jsx
@@ -6,6 +6,8 @@ import style from './AdminOrderItem.module.scss';
 export default function AdminOrderItem({ item, idx }) {
   const navigate = useNavigate();
 
+  const test = [];
+
   return (
     <li key={item.id} className={style.container}>
       <input type='checkbox' name='' id='' />
