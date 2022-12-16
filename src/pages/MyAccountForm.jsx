a894230a9ac4581ce69a41990ae4f69f5516b7d1
soyun <soyoon4122@naver.com>
2022-12-16 14:36:19 +0900

    Update MyAccountForm

--- src/pages/MyAccountForm.jsx
+++ src/pages/MyAccountForm.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getListBank } from '@/api/requests';
+import style from './MyAccountForm.module.scss';
 
 export default function MyAccountForm() {
-  return <div>MyAccountForm</div>;
+  const [banks, setBanks] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const data = await getListBank();
+      setBanks(data);
+    }
+    getData();
+  }, []);
+
+  return (
+    <div className={style.accountAddForm}>
+      <h3>계좌 추가</h3>
+      <ul>
+      {
+        banks.map((item, idx) => {
+          return  <li item={item} key={idx}>
+                    <h4>{item.name}</h4>
+                  </li>
+        })
+      }
+      </ul>
+    </div>
+  )
 }
