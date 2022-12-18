995cafa996e42401df5143111f483365f604e031
jisooround <9utesleben@gmail.com>
2022-12-19 01:59:20 +0900

    Update CartList empty

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import CartItem from '../components/CartItem';
 import style from './CartList.module.scss';
 import { useSelector } from 'react-redux';
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
+import { BsCart2 } from 'react-icons/bs';
 
 export default function CartList() {
   let list = useSelector((state) => state.cart);
+  const navigate = useNavigate();
   console.log(list);
 
   // 체크 된 항목 찾기
@@ -13,10 +17,18 @@ export default function CartList() {
   
   return (
     <div className={style.cartlist}>
-        {
+        { list.length > 0 ? 
         list.map((item) => {
           return <CartItem key={item.id} item={item} />;
-        })}
+        }) :
+        <div className={style.empty}>
+          <BsCart2 size='30' title='장바구니' color='rgb(95, 0, 128)' />
+          <p>장바구니가 비었습니다.</p>
+          <Button name={'쇼핑하러 가기'} onClick={() => {
+        navigate('/');
+      }}/>
+        </div>
+        }
       </div>
         );
 }
