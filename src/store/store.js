8f8e87cda8624372a83bd353c31dba5ac639b019
autumnly1007 <autumnly1007@gmail.com>
2022-12-18 13:48:13 +0900

    Delete stock slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cart from './cartSlice.js';
-import stock from './stockSlice.js';
 import user from './userSlice.js';
 import loading from './loadingSlice.js';
 import like from './likeSlice.js';
@@ -8,20 +7,11 @@ import like from './likeSlice.js';
 // persist
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
-import {
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const reducers = combineReducers({
   cart: cart.reducer,
   user: user.reducer,
-  stock: stock.reducer,
   loading: loading.reducer,
   like: like.reducer,
 });
@@ -30,7 +20,7 @@ const persistConfig = {
   key: 'root',
   storage, // 저장 공간
   whitelist: ['cart'], // 유지하고 싶은 값
-  blacklist: ['stock', 'loading', 'like'] // 유지하지 않을 내용
+  blacklist: ['loading', 'like'], // 유지하지 않을 내용
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
@@ -42,16 +32,15 @@ export default configureStore({
   // state 를 여기에 등록해야 사용 가능
   reducer: {
     user: user.reducer,
-    stock: stock.reducer,
     cart: cart.reducer,
     loading: loading.reducer,
     like: like.reducer,
     persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware({
+    getDefaultMiddleware({
       serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-  }),
+    }),
 });
