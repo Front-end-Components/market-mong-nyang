8c2190dbd56fdd856dcce60f26936ab28e53263a
jisooround <9utesleben@gmail.com>
2022-12-18 21:41:59 +0900

    Fix check box

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -10,16 +10,13 @@ export default function CartList() {
   // 체크 된 항목 찾기
   const checkedList = list.filter(item => item.checked === true);
   console.log('checkedList :', checkedList);
-
+  
   return (
     <div className={style.cartlist}>
-        {/* <button className={style.delbtn}>선택 삭제</button>
-        <button className={style.alldelbtn}>전체 삭제</button> */}
         {
         list.map((item) => {
           return <CartItem key={item.id} item={item} />;
         })}
       </div>
-    </div>
   );
 }
