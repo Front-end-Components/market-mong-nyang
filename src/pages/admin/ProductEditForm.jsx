09562f4554e25d9f219e454a42d84c8aa1f8e37c
autumnly1007 <autumnly1007@gmail.com>
2022-12-18 14:23:09 +0900

    Add try catch finally

--- src/pages/admin/ProductEditForm.jsx
+++ src/pages/admin/ProductEditForm.jsx
@@ -1,15 +1,16 @@
 import { updateProduct } from '@/api/requests';
+import { hideLoading, showLoading } from '@/store/loadingSlice';
+import { useDispatch } from 'react-redux';
 import Button from '@/components/Button';
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import style from './ProductEditForm.module.scss';
 
 export default function ProductEditForm() {
   const navigate = useNavigate();
+  const { id } = useParams();
   const [product, setProduct] = useState({});
-  const [isUploading, setIsUploading] = useState(false);
-  const [success, setSuccess] = useState();
-
+  const dispatch = useDispatch();
   const { state } = useLocation();
 
   useEffect(() => {
@@ -37,71 +38,95 @@ export default function ProductEditForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(product);
-    if (window.confirm('상품을 등록하시겠습니까?')) {
-      updateProduct(product.id, product).then((res) => {
-        if (res) {
-          alert('상품 등록이 완료되었습니다.');
-          navigate('/admin/products');
-        } else {
-          alert('상품 등록이 완료되지 못했습니다.');
-        }
-      });
+    if (window.confirm('상품을 수정하시겠습니까?')) {
+      try {
+        dispatch(showLoading());
+        updateProduct(product.id, product);
+        alert('상품 수정이 완료되었습니다.');
+        navigate('/admin/products');
+      } catch {
+        alert('상품 수정이 완료되지 못했습니다.');
+      } finally {
+        dispatch(hideLoading());
+      }
     }
   };
+
+  const handleBack = () => {};
 
   return (
     <div className={style.formWrap}>
       <div className={style.header}>
         <h1>상품 수정</h1>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className={style.inputWrap}>
           <span>제품명 *</span>
-          <input type='text' name='title' value={product.title ?? ''} placeholder='제품명' required onChange={handleChange} />
+          <input
+            type="text"
+            name="title"
+            value={product.title ?? ''}
+            placeholder="제품명"
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className={style.inputWrap}>
           <span>가격 *</span>
-          <input type='number' name='price' value={product.price ?? ''} placeholder='가격' required onChange={handleChange} />
+          <input
+            type="number"
+            name="price"
+            value={product.price ?? ''}
+            placeholder="가격"
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className={style.inputWrap}>
           <div className={style.textWrap}>
             <span>제품 상세 설명 *</span>
           </div>
-          <textarea type='text' name='description' value={product.description ?? ''} placeholder='제품 상세 설명' required onChange={handleChange} />
+          <textarea
+            type="text"
+            name="description"
+            value={product.description ?? ''}
+            placeholder="제품 상세 설명"
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className={style.inputWrap}>
           <span>카테고리 *</span>
-          <select name='tags' onChange={handleChange} value={product.tags} required>
-            <option value=''>선택</option>
-            <option value='주식'>주식</option>
-            <option value='간식'>간식</option>
-            <option value='건강'>건강</option>
-            <option value='케어'>케어</option>
-            <option value='의류'>의류</option>
-            <option value='리빙'>리빙</option>
-            <option value='외출'>외출</option>
-            <option value='위생'>위생</option>
+          <select name="tags" onChange={handleChange} value={product.tags} required>
+            <option value="">선택</option>
+            <option value="주식">주식</option>
+            <option value="간식">간식</option>
+            <option value="건강">건강</option>
+            <option value="케어">케어</option>
+            <option value="의류">의류</option>
+            <option value="리빙">리빙</option>
+            <option value="외출">외출</option>
+            <option value="위생">위생</option>
           </select>
         </div>
         <div className={style.inputWrap}>
           <span>썸네일 이미지</span>
-          <input type='file' accept='image/*' name='thumbnailBase64' onChange={handleChange} />
+          <input type="file" accept="image/*" name="thumbnailBase64" onChange={handleChange} />
         </div>
         <div className={style.inputWrap}>
           <span>상품 상세 이미지</span>
-          <input type='file' accept='image/*' name='photoBase64' onChange={handleChange} />
+          <input type="file" accept="image/*" name="photoBase64" onChange={handleChange} />
         </div>
         <div className={style.inputWrap}>
           <span>품절 여부</span>
-          <select name='isSoldOut' onChange={handleChange} value={product.isSoldOut}>
+          <select name="isSoldOut" onChange={handleChange} value={product.isSoldOut}>
             <option value={true}>Y</option>
             <option value={false}>N</option>
           </select>
         </div>
         <div className={style.buttons}>
           <Button name={'수정완료'} isPurple={true} onClick={handleSubmit} />
-          <Button name={'취소'} onClick={() => navigate(-1)} />
+          <Button name={'취소'} onClick={() => navigate(`/admin/product/${id}`)} />
         </div>
       </form>
     </div>
