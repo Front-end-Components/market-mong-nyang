b0b1c8c8316c6e6581bff67adb9940c7cc20d5d3
autumnly1007 <autumnly1007@gmail.com>
2022-12-18 18:29:51 +0900

    Update NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
+import Button from '@/components/Button';
 import React from 'react';
+import { useNavigate } from 'react-router';
+import style from './NotFound.module.scss';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={style.notFound}>
+      <p>페이지를 찾을 수 없습니다.</p>
+      <Button isPurple={true} name={'뒤로가기'} onClick={() => navigate(-1)} />
+    </div>
+  );
 }
