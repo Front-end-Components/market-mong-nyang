c59404b57c5c774c6c7b5391d2157448933a2fde
soyun <soyoon4122@naver.com>
2022-12-20 14:27:03 +0900

    Update like fix & clear

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -8,8 +8,8 @@ import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { formatPrice } from '@/utils/formats.js';
 import { insertItem } from '@/store/cartSlice';
-import { insertLike } from '@/store/likeSlice';
-import { useDispatch } from "react-redux";
+import { insertLike, deleteLike } from '@/store/likeSlice';
+import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from '@/store/loadingSlice';
 
 export default function ProductDetail() {
@@ -21,6 +21,8 @@ export default function ProductDetail() {
   let dispatch = useDispatch();
   const navigate = useNavigate();
   const [like, setLike] = useState(false);
+  let list = useSelector((state) => state.like);
+  const heartOn = list.map((item => item.id)).find((id) => id === products.id) === products.id;
 
   useEffect(() => {
     async function getData() {
@@ -61,19 +63,22 @@ export default function ProductDetail() {
               <span
                 className={style.like}
                 onClick={()=> {
-                  setLike(!like);
-                  dispatch(insertLike({
-                    id: products.id,
-                    isSoldOut: products.isSoldOut,
-                    price: products.price,
-                    thumbnail: products.thumbnail,
-                    title: products.title,
-                    count: count,
-                    checked: true,
-                  }));
+                  setLike(!false)
+                  if(heartOn === true) dispatch(deleteLike(products.id))
+                  if(heartOn === false) {
+                    dispatch(insertLike({
+                      id: products.id,
+                      isSoldOut: products.isSoldOut,
+                      price: products.price,
+                      thumbnail: products.thumbnail,
+                      title: products.title,
+                      count: count,
+                      checked: true,
+                    }));
+                  }
                 }}
               >
-                <Like like={like} setLike={setLike} />
+                <Like heartOn={heartOn} like={like} setLike={setLike} />
               </span>
             </div>
             <div className={style.total}>
