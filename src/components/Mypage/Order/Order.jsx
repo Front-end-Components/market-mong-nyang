24e071edf0b42b29cae9b0b5028cd1cc23057968
plou102 <plou102@naver.com>
2022-12-28 18:49:07 +0900

    update order

--- src/components/Mypage/Order/Order.jsx
+++ src/components/Mypage/Order/Order.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Button from '@/components/common/Button';
 import { useNavigate } from 'react-router-dom';
 import style from '@/pages/Mypage/MyOrder/MyOrder.module.scss';
@@ -9,22 +9,41 @@ import { Modal } from '@/components/common/Modal';
 export default function Order({ item }) {
   const [modal, setModal] = useState(false);
   const [modalText, setModalText] = useState('');
+  const [displayBtn, setDisplayBtn] = useState(false);
+  const [stateText, setStateText] = useState('구매 완료');
+  const [guideMain, setGuideMain] = useState('구매가 완료 되었습니다.');
+  const [guideSub, setGuideSub] = useState('구매 확정 이후에는 주문 취소가 불가능합니다.');
   const navigate = useNavigate();
 
-  let orderGuideMain = '구매가 완료 되었습니다.';
-  let orderGuideSub = '구매 확정 이후에는 주문 취소가 불가능합니다.';
-  if (item.isCanceled) {
-    orderGuideMain = '주문이 취소 되었습니다.';
-    orderGuideSub = '';
-  } else if (item.done) {
-    orderGuideMain = '구매가 확정 되었습니다.';
+  useEffect(() => {
+    if (item.isCanceled) {
+      setGuideMain('주문이 취소 되었습니다.');
+      setGuideSub('');
+      setStateText('주문 취소');
+    } else if (item.done) {
+      setGuideMain('구매가 확정 되었습니다.');
+      setStateText('구매 확정');
+    }
+  }, []);
+
+  async function OrderOk () {
+    while(item.cnt > 0) {
+      const detailID = {
+        detailId: item[item.cnt],
+      };
+      await updateOrderOk(detailID);
+      item.cnt--;
+    }
   }
 
-  let stateText = '구매 완료';
-  if (item.done) {
-    stateText = '구매 확정';
-  } else if (item.isCanceled) {
-    stateText = '주문 취소';
+  async function OrderCancel () {
+    while(item.cnt > 0) {
+      const detailID = {
+        detailId: item[item.cnt],
+      };
+      await updateOrderCancel(detailID);
+      item.cnt--;
+    }
   }
 
   let date = item.timePaid.substr(0, 10);
@@ -55,48 +74,42 @@ export default function Order({ item }) {
           <p className={style.orderPrice}>{price}원</p>
           <p className={style.orderDate}>{date}</p>
           <p className={style.orderState}>{stateText}</p>
-          <p className={style.orderGuide}>{orderGuideMain}</p>
-          <p className={style.orderGuide}>{orderGuideSub}</p>
+          <p className={style.orderGuide}>{guideMain}</p>
+          <p className={style.orderGuide}>{guideSub}</p>
         </div>
         <div className={style.btnContent}>
           <Button
-            display={item.done || item.isCanceled}
+            display={displayBtn || item.isCanceled || item.done}
             name={'주문 취소'}
             onClick={() => {
               if (window.confirm('주문을 취소 하시겠습니까?')) {
                 try {
-                  while (item.cnt > 0) {
-                    const detailID = {
-                      detailId: item[item.cnt],
-                    };
-                    updateOrderCancel(detailID);
-                    item.cnt--;
-                  }
+                  OrderCancel();
                 } finally {
-                  window.location.replace('/mypage/order');
                   setModal(true);
                   setModalText('주문이 취소 되었습니다.');
+                  setDisplayBtn(true);
+                  setGuideMain('주문이 취소 되었습니다.');
+                  setGuideSub('');
+                  setStateText('주문 취소');
                 }
               }
             }}
           />
           <Button
-            display={item.done || item.isCanceled}
+            display={displayBtn || item.isCanceled || item.done}
             name={'구매 확정'}
             isPurple={true}
             onClick={() => {
               if (window.confirm('구매를 확정 하시겠습니까?')) {
                 try {
-                  for (let i = 1; i <= item.cnt; i++) {
-                    const detailID = {
-                      detailId: item[i],
-                    };
-                    updateOrderOk(detailID);
-                  }
+                  OrderOk();
                 } finally {
-                  window.location.replace('/mypage/order');
                   setModal(true);
                   setModalText('구매가 확정 되었습니다.');
+                  setDisplayBtn(true);
+                  setGuideMain('구매가 확정 되었습니다.');
+                  setStateText('구매 확정');
                 }
               }
             }}
