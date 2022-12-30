4a9df7a3312c01740395e637b0e0ac57689120ff
plou102 <plou102@naver.com>
2022-12-31 02:26:58 +0900

    update datePicker

--- src/pages/Mypage/MyOrder/MyOrder.jsx
+++ src/pages/Mypage/MyOrder/MyOrder.jsx
@@ -5,6 +5,7 @@ import { getListOrder } from '@/api/requests';
 import Order from '@/components/Mypage/Order/Order';
 import { GrPowerReset } from 'react-icons/gr';
 import DatePicker from 'react-multi-date-picker';
+import InputIcon from "react-multi-date-picker/components/input_icon"
 import { formatDate } from '@/utils/formats';
 import { useDispatch } from 'react-redux';
 import { hideLoading, showLoading } from '@/store/loadingSlice';
@@ -45,11 +46,11 @@ export default function MyOrder() {
     getData();
   }, [page]);
 
-  const CustomInput = ({ value, onClick }) => (
-    <button className={style.customInput} onClick={onClick}>
-      {value ? value : `거래일자 선택`}
-    </button>
-  );
+  useEffect(() => {
+    if (date === '') {
+      handleSearch();
+    }
+  }, [date]);
 
   const handleSearch = (newDate) => {
     const selectDate = newDate ? formatDate(newDate) : date ? formatDate(date) : '';
@@ -96,11 +97,7 @@ export default function MyOrder() {
     setDate('');
   };
 
-  useEffect(() => {
-    if (date === '') {
-      handleSearch();
-    }
-  }, [date]);
+
 
   return (
     <div className={style.myOrder}>
@@ -108,11 +105,10 @@ export default function MyOrder() {
       {modal ? <Modal modalText={modalText} /> : null}
       <div className={style.datePickerWrap}>
         <DatePicker
-          className="purple"
-          selected={date}
+          className='purple'
+          value={date}
           onChange={handleDatePicker}
-          dateFormat="yyyy.MM.dd"
-          customInput={<CustomInput />}
+          render={<InputIcon />}
         />
       </div>
       <button className={style.searchReset} onClick={handleReset}>
